cd13b16ec8301a04ff09d44b0727b52c111f6697
imthanhhai217@gmail.com <Haihai97@>
2023-04-25 20:55:58 +0700

    Initial commit

---
+++ app/src/main/java/com/vndevpro/android52_day2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vndevpro.android52_day2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonebook_linearlayout);
+    }
+}
